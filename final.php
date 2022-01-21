5ded3ed72e571365a75cd8254d6d0f74974ff88c
Divyadarshan soni <86970594+Divyasoniworld@users.noreply.github.com>
2022-01-21 14:51:42 +0530

    Add files via upload

---
+++ final.php
@@ -0,0 +1,33 @@
+
+<?php
+// error_reporting(0);
+
+require "vendor/autoload.php";
+ $name=$_POST["name"];
+ $emaill=trim($_POST["email"]);
+ $sub=$_POST["subject"];
+ $msg=$_POST["message"];
+ $email = new \SendGrid\Mail\Mail();
+ $email->setFrom("sonidev7250@gmail.com");
+ $email->setSubject($sub);
+ $email->addTo("divyasoniworld@gmail.com");
+ $email->addContent("text/html","<div>".$name."<br>".$emaill."<br>".$msg."</div>");
+
+ $sendgrid = new SendGrid("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
+ ");
+ 
+
+
+ if($sendgrid->send($email))
+ {
+     
+     
+     echo '<script>alert("Mail Sent Successfully");
+     window.location.replace("http://localhost/portfolio2/");</script>';
+
+
+    
+}
+ else{ echo '<script>alert("Something Went Wrong")</script>';}
+
+?>
---
+++ script.js
@@ -0,0 +1,30 @@
+let menu = document.querySelector('#menu-btn');
+let header = document.querySelector('.header');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    header.classList.toggle('active');
+}
+
+var typed = new Typed(".typing", {
+    strings: ["Front-End Developer", "Web Developer", "Backend Developer", "Wordpress Developer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    header.classList.remove('active');
+}
+
+let themeToggler = document.querySelector('#theme-toggler');
+
+themeToggler.onclick = () =>{
+    themeToggler.classList.toggle('fa-sun');
+    if(themeToggler.classList.contains('fa-sun')){
+        document.body.classList.add('active');
+    }else{
+        document.body.classList.remove('active');
+    }
+}
